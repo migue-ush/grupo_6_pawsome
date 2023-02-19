7a545804c0d5183e2b0d04feed05491509b504db
migue-ush <albornozmiguel@gmail.com>
2023-02-19 16:39:05 -0300

    login

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,7 +7,7 @@ router.get('/register', userController.register);
 router.post('/register', userController.register);
 
 router.get('/login', userController.login);
-router.post('login', userController.loginProcess);
+router.post('/login', userController.loginProcess);
 router.get('/profile/:userId', userController.profile);
 
 module.exports = router;
