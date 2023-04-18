e4d2ce06e2e76315c6fb9687eed616d6250968fb
carovicino <carolinavicino97@gmail.com>
2023-04-18 11:43:34 -0300

    users y userprofile styles

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": "",
-    "database": "pawsome_db",
+    "database": "structure",
     "host": "127.0.0.1",
     "port": "3306",
     "dialect": "mysql"
