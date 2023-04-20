f55f0242cf679fb4295e797b7c9a40c484c72280
migue-ush <albornozmiguel@gmail.com>
2023-04-19 23:54:16 -0300

    agregando botones a vista perfil de usuario

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,11 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-<<<<<<< HEAD
-    "password": "",
-=======
     "password": "root",
->>>>>>> 4105959 (continuando con CRUD para usuarios)
     "database": "structure",
     "host": "127.0.0.1",
     "port": "8889",
